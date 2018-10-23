9458fd8d17aba64d3d3d1f552e0ac1db422d314b
steffanpaul <steffanpaul@college.harvard.edu>
2018-10-22 22:12:08 -0400

    started resbind transfer learning trials

--- toypk/transferlearning/implement.py
+++ toypk/transferlearning/implement.py
@@ -2,6 +2,6 @@ import os
 from subprocess import call
 
 
-call(['python', 'mlp_pkhp.py', '--train', '--test', '--fom', '--somcalc', '--somvis'])
-call(['python', 'mlp_pkhp.py', '--transfer', '--train', '--test', '--fom', '--somcalc', '--somvis'])
+call(['python', 'resbind_pkhp.py', '--train', '--test', '--fom', '--somcalc', '--somvis'])
+call(['python', 'resbind_pkhp.py', '--transfer', '--train', '--test', '--fom', '--somcalc', '--somvis'])
 
