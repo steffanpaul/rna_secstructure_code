a9eab56e7e7fa0b8f84e3dce19914905eccd6e85
steffanpaul <steffanpaul@college.harvard.edu>
2018-10-23 17:17:15 -0400

    needsfix

--- toypk/transferlearning/implement.py
+++ toypk/transferlearning/implement.py
@@ -3,17 +3,17 @@ from subprocess import call
 
 #The first and second number after the filename are the datatype and the trial number
 
-for data in ['1','2']
+for data in ['1','2']:
 	#train on just pkhp
-	call(['python', 'resbind_pkhp.py', data, '1', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+	call(['python', 'mlp_pkhp.py', data, '1', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
 
 	#train on all hp and then transfer to all pkhp
-	call(['python', 'resbind_pkhp.py', data, '2', '--train', '--test', '--somcalc', '--somvis'])
-	call(['python', 'resbind_pkhp.py', data, '2', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
+	call(['python', 'mlp_pkhp.py', data, '2', '--train', '--test', '--somcalc', '--somvis'])
+	call(['python', 'mlp_pkhp.py', data, '2', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
 
 	#train on some pkhp
-	call(['python', 'resbind_pkhp.py', data, '3', '--some', '--train', '--test', '--somcalc', '--somvis'])
+	call(['python', 'mlp_pkhp.py', data, '3', '--some', '--train', '--test', '--somcalc', '--somvis'])
 
 	#train on all hp and then transfer to some pkhp
-	call(['python', 'resbind_pkhp.py', data, '4', '--train', '--test', '--somcalc', '--somvis'])
-	call(['python', 'resbind_pkhp.py', data, '4', '--some', '--transfser', '--train', '--test', '--somcalc', '--somvis'])
+	call(['python', 'mlp_pkhp.py', data, '4', '--train', '--test', '--somcalc', '--somvis'])
+	call(['python', 'mlp_pkhp.py', data, '4', '--some', '--transfser', '--train', '--test', '--somcalc', '--somvis'])
