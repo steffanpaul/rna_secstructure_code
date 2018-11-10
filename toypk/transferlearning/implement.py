eb856f4936b280d0e94d57e576dc0c75b1eb7b81
steffanpaul <steffanpaul@college.harvard.edu>
2018-11-09 22:08:04 -0500

    toypk tf trials

--- toypk/transferlearning/implement.py
+++ toypk/transferlearning/implement.py
@@ -4,19 +4,20 @@ from subprocess import call
 #The first and second number after the filename are the datatype and the trial number
 
 for data in ['4']:
-	call(['python', 'mlp_pkhp.py', data, '1', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
-	#call(['python', 'mlp_pkhp.py', data, '1.3', '--some' '50', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
-	#call(['python', 'mlp_pkhp.py', data, '1.4', '--some' '100', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '0', '--pretransfer', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '1', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py', data, '1.3', '--some' '50', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py', data, '1.4', '--some' '100', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
 
-	call(['python', 'mlp_pkhp.py', data, '2', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '2', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
 
-	call(['python', 'mlp_pkhp.py', data, '3',  '--some', '50', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '3',  '--some', '50', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
 
-	call(['python', 'mlp_pkhp.py', data, '4',  '--some', '100', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '4',  '--some', '100', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
 
-	call(['python', 'mlp_pkhp.py', data, '5',  '--some', '200', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '5',  '--some', '200', '--transfer', '--train', '--test', '--somcalc', '--somvis'])
 
-	'''
+'''
 	#train on just pkhp
 	call(['python', 'mlp_pkhp.py', data, '1', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
 
