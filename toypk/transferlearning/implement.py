060ff86b5082dbbe526b61d7a73d1e24c4f13b22
steffanpaul <steffanpaul@college.harvard.edu>
2018-11-09 23:58:22 -0500

    added d4t1.5 to toypk tf

--- toypk/transferlearning/implement.py
+++ toypk/transferlearning/implement.py
@@ -6,16 +6,17 @@ from subprocess import call
 for data in ['4']:
     #call(['python', 'mlp_pkhp.py', data, '0', '--pretransfer', '--train', '--test', '--somcalc', '--somvis'])
     #call(['python', 'mlp_pkhp.py', data, '1', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
-    call(['python', 'mlp_pkhp.py' , data, '1.3', '--some' '50', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
-    call(['python', 'mlp_pkhp.py', data, '1.4', '--some' '100', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py' , data, '1.3', '--some' '50', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py', data, '1.4', '--some' '100', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
+    call(['python', 'mlp_pkhp.py', data, '1.5', '--some' '200', '--justpkhp', '--train', '--test', '--somcalc', '--somvis'])
 
-    call(['python', 'mlp_pkhp.py', data, '2.1', '--transfer', '--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py', data, '2.1', '--transfer', '--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
 
-    call(['python', 'mlp_pkhp.py', data, '3.1',  '--some', '50', '--transfer','--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py', data, '3.1',  '--some', '50', '--transfer','--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
 
-    call(['python', 'mlp_pkhp.py', data, '4.1',  '--some', '100', '--transfer','--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
+    #call(['python', 'mlp_pkhp.py', data, '4.1',  '--some', '100', '--transfer','--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
 
-    call(['python', 'mlp_pkhp.py', data, '5.1',  '--some', '200', '--transfer','--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
+    #all(['python', 'mlp_pkhp.py', data, '5.1',  '--some', '200', '--transfer','--setepochs', '1000', '--train', '--test', '--somcalc', '--somvis'])
 
 '''
 	#train on just pkhp
