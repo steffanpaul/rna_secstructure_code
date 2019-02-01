22a5d5f0b46f7385d588ec671f194b790b3b3c46
steffanpaul <steffanpaul@college.harvard.edu>
2019-02-01 16:03:00 -0500

    tried using bpm for background

--- marks/implement.py
+++ marks/implement.py
@@ -8,9 +8,12 @@ families = ['RF00002', 'RF00005', 'RF00010', 'RF00017', 'RF00023', 'RF00050',
 rnn_tests_full = ['RF00005']
 rnn_tests_som = ['RF00059', 'RF00174', 'RF00005']
 
-for fam in rnn_tests_som:
-    call(['python', 'rnn_marks.py', fam, '--somcalc', '--somvis', '--write'])
+#for fam in rnn_tests_som:
+#    call(['python', 'rnn_marks.py', fam, '--somcalc', '--somvis', '--write'])
 
 #for fam in rnn_tests_full:
 #    call(['python', 'rnn_marks.py', fam, '--train', '--somcalc', '--somvis', '--write'])
     #call(['python', 'test.py', fam])
+
+call(['python', 'mlp_marks_bpm.py', 'RF00380', 'red', '512', '--train', '--test', '--somcalc', '--somvis'])
+call(['python', 'mlp_marks_bpm.py', 'RF00380', 'bpm', '512', '--train', '--test', '--somcalc', '--somvis', '--write'])
